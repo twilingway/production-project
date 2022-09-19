321e6d98b6e685f9d73a2c1e7e914fd5df68ea35
GLAZUNOV ALEKSEI <993468@gmail.com>
2022-09-19 22:14:37 +0400

    10 Архитектура. Начинаем внедрять. Основы

--- config/build/buildResolvers.ts
+++ config/build/buildResolvers.ts
@@ -1,7 +1,12 @@
+import { IBuildOptions } from "./types/config";
 import { ResolveOptions } from "webpack";
 
-export function buildResolvers(): ResolveOptions {
+export function buildResolvers(option: IBuildOptions): ResolveOptions {
   return {
     extensions: [".tsx", ".ts", ".js"],
+    preferAbsolute: true,
+    modules: [option.paths.src, "node_modules"],
+    mainFiles: ["index"],
+    alias: {},
   };
 }
---
+++ src/app/App.tsx
@@ -0,0 +1,29 @@
+import { useTheme } from "app/providers/ThemeProvider";
+import { classNames } from "shared/config/lib/classNames/classNames";
+import { AboutPage } from "pages/AboutPage";
+import { MainPage } from "pages/MainPage";
+import { Suspense } from "react";
+import { Link, Route, Routes } from "react-router-dom";
+
+import "./styles/index.scss";
+
+const App = () => {
+  const { theme, toggleTheme } = useTheme();
+
+  return (
+    <div className={classNames("app", {}, [theme])}>
+      <button onClick={toggleTheme}>TOGGLE</button>
+      <Link to="/">Главная</Link>
+      <Link to="/about">О сайте</Link>
+
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/" element={<MainPage />} />
+        </Routes>
+      </Suspense>
+    </div>
+  );
+};
+
+export default App;
