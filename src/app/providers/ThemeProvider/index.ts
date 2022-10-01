f54b90a5595e01b9e4a215292880c9315eb5b320
GLAZUNOV ALEKSEI <993468@gmail.com>
2022-10-01 23:34:47 +0400

    fix

--- src/app/providers/ThemeProvider/index.ts
+++ src/app/providers/ThemeProvider/index.ts
@@ -2,6 +2,6 @@ import ThemeProvider from './ui/ThemeProvider';
 import { useTheme } from './lib/useTheme';
 import { Theme } from './lib/ThemeContext';
 
-export {
-  ThemeProvider, useTheme, Theme
-};
+const temp = 5;
+
+export { ThemeProvider, useTheme, Theme, temp };
