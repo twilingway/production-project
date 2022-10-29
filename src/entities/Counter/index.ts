498a6cb283097b239598dfbc09eabc1d9c7ac2fb
GLAZUNOV ALEKSEI <993468@gmail.com>
2022-10-29 13:06:13 +0400

    fix

--- src/entities/Counter/index.ts
+++ src/entities/Counter/index.ts
@@ -1,4 +1,4 @@
-import { counterReducer } from './model/slice/counterSLice';
+import { counterReducer } from './model/slice/counterSlice';
 import type { CounterSchema } from './model/types/counterSchema';
 import { Counter } from './ui/Counter';
 
