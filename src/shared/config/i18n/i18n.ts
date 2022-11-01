44883cb54476c1e2d942d053a163193662e87992
GLAZUNOV ALEKSEI <993468@gmail.com>
2022-11-01 18:28:47 +0400

    fix

--- src/shared/config/i18n/i18n.ts
+++ src/shared/config/i18n/i18n.ts
@@ -4,7 +4,7 @@ import { initReactI18next } from 'react-i18next';
 import HttpBackend from 'i18next-http-backend';
 import LanguageDetector from 'i18next-browser-languagedetector';
 
-const PUBLIC_PATH = __IS_DEV__ ? '../' : process.env.PUBLIC_URL;
+const PUBLIC_PATH = __IS_DEV__ ? '..' : process.env.PUBLIC_URL;
 
 i18n
 
@@ -21,7 +21,7 @@ i18n
     },
     lowerCaseLng: true,
     // backend: { loadPath: '/locales/{{lng}}/{{ns}}.json' },
-    backend: { loadPath: `${PUBLIC_PATH}locales/{{lng}}/{{ns}}.json` },
+    backend: { loadPath: `${PUBLIC_PATH}/locales/{{lng}}/{{ns}}.json` },
   });
 
 export default i18n;
