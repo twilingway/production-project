829c152942a01841e0ab0f7491a9cc719194968b
GLAZUNOV ALEKSEI <993468@gmail.com>
2022-11-01 18:19:00 +0400

    edit i18n

--- src/shared/config/i18n/i18n.ts
+++ src/shared/config/i18n/i18n.ts
@@ -1,12 +1,14 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 
-import Backend from 'i18next-http-backend';
+import HttpBackend from 'i18next-http-backend';
 import LanguageDetector from 'i18next-browser-languagedetector';
+
+const PUBLIC_PATH = __IS_DEV__ ? '../public' : process.env.PUBLIC_URL;
 
 i18n
 
-  .use(Backend)
+  .use(HttpBackend)
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
@@ -17,8 +19,9 @@ i18n
     interpolation: {
       escapeValue: false, // not needed for react as it escapes by default
     },
+    lowerCaseLng: true,
     // backend: { loadPath: '/locales/{{lng}}/{{ns}}.json' },
-    backend: { loadPath: '../locales/{{lng}}/{{ns}}.json' },
+    backend: { loadPath: `${PUBLIC_PATH}/locales/{{lng}}/{{ns}}.json` },
   });
 
 export default i18n;
