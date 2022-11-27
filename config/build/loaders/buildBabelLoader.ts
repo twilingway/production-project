9021fcacf40dead7553691b117d8d128424c198c
GLAZUNOV ALEKSEI <993468@gmail.com>
2022-11-27 19:03:56 +0400

    add refresh plugin

---
+++ config/build/loaders/buildBabelLoader.ts
@@ -0,0 +1,15 @@
+import { IBuildOptions } from '../types/config';
+
+export function buildBabelLoader(options: IBuildOptions) {
+  return {
+    test: /\.(js|jsx|tsx)$/,
+    exclude: /(node_modules)/,
+    use: {
+      loader: 'babel-loader',
+      options: {
+        presets: ['@babel/preset-env'],
+        plugins: [options.isDev && require.resolve('react-refresh/babel')].filter(Boolean),
+      },
+    },
+  };
+}
